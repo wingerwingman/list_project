f51fc0b5c4e878a2a1a6dd63ae7f7d31b0e5dc8c
Justin Winger <justinwinger4@gmail.com>
2020-06-22 13:57:31 -0600

    Validate category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
     has_many :lists
     has_many :users, through: :lists  
     validates :name, presence: true
-    # validates_uniqueness_of :name
+    validates_uniqueness_of :name
 
     
 end
