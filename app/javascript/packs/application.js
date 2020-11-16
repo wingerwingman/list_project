9712725b711214569ee7e084e107e9ce70a69004
Justin Winger <justinwinger4@gmail.com>
2020-11-16 12:17:56 -0700

    event

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
