ca10908f720d08babe57d5c847ecd8b119f83b61
Justin Winger <justinwinger4@gmail.com>
2020-11-17 08:34:22 -0700

    turbolinks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
   config.assets.js_compressor = :uglifier
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  #config.assets.compile = true
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
