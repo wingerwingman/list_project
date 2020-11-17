083c1c8868468bda7e694e2039815ff8458c3a50
Justin Winger <justinwinger4@gmail.com>
2020-11-17 08:58:54 -0700

    turbolinks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # end
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_controller.relative_url_root = '/app'
+ 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
